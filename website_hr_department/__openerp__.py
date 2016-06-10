782c58667ae9996d3de8e100c7792852fa5d6fb7
Serpent Consulting Services Pvt Ltd <serpentcs.lp@gmail.com>
2016-06-10 20:26:52 +0530

    Corrections

--- website_hr_department/__openerp__.py
+++ website_hr_department/__openerp__.py
@@ -4,6 +4,8 @@
 #     This file is part of website_hr_department, an Odoo module.
 #
 #     Copyright (c) 2015 ACSONE SA/NV (<http://acsone.eu>)
+#     Copyright (c) 2016 Serpent Consulting Services Pvt. Ltd.
+#                       (<http://www.serpentcs.com>)
 #
 #     website_hr_department is free software: you can redistribute it and/or
 #     modify it under the terms of the GNU Affero General Public License
@@ -26,10 +28,11 @@
     'summary': """
         Display the structure of your departments and their members.
         """,
-    'author': 'ACSONE SA/NV, '
-            'Odoo Community Association (OCA). '
-            'Serpent Consulting Services Pvt. Ltd.',
-    'website': "http://acsone.eu,serpentcs.com",
+    'author': "ACSONE SA/NV, "
+              "Odoo Community Association (OCA), "
+              "Serpent Consulting Services Pvt. Ltd. ",
+             
+    'website': "http://acsone.eu, http://www.serpentcs.com",
     'category': 'Website',
     'version': '9.0.1.0.0',
     'license': 'AGPL-3',
